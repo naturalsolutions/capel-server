0560b52927d78de2f88684835ef338c9cc01d7f9
patkap <patkap@users.noreply.github.com>
2018-04-24 14:13:46 +0200

    Update SQLALCHEMY_DATABASE_URI in app_conf.py with value from heroku config DATABASE_URL.

--- app_conf.py
+++ app_conf.py
@@ -1,7 +1,6 @@
-from credentials import dbcredentials as dba
 from datetime import timedelta
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{dba}localhost/capel'
+SQLALCHEMY_DATABASE_URI = 'postgres://nprxexbyoznabo:9a18f5b097e560f1bad29b559e6c4d20fff688c74cd747ff0851b28455377923@ec2-107-20-151-189.compute-1.amazonaws.com:5432/de9j10rfk0jqgi'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWTSECRET = b'abcdef'
 JWT_AUTH_TYPE = 'Bearer'
