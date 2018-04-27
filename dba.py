f48e4a212e9a35290c4b5418d886898c7bb93196
patkap <patkap@users.noreply.github.com>
2018-04-27 18:24:44 +0200

    bug hunting: 300sec id token.

---
+++ dba.py
@@ -0,0 +1,2 @@
+DATABASE_URL = 'postgres://nprxexbyoznabo:9a18f5b097e560f1bad29b559e6c4d20fff688c74cd747ff0851b28455377923@ec2-107-20-151-189.compute-1.amazonaws.com:5432/de9j10rfk0jqgi'  # noqa
+SENDGRID_KEY = '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'  # noqa
