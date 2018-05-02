7673668c63ba75700e71252d8a24faa66ec4c730
patkap <patkap@users.noreply.github.com>
2018-05-02 17:00:59 +0200

    Leverage environment variable for API key.

--- app_conf.py
+++ app_conf.py
@@ -1,9 +1,5 @@
+import os
 from datetime import timedelta
-try:
-    from dba import DATABASE_URL, SENDGRID_KEY
-except Exception:
-    DATABASE_URL = f'postgresql://postgres@localhost/capel'
-    SENDGRID_KEY = 'Please fill in your sendgrid api key.'  # TODO: raise
 
 SQLALCHEMY_DATABASE_URI = DATABASE_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -14,6 +10,6 @@ VALID_PWD_MIN_LEN = 6
 VALID_EMAIL_REGEX = r'^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$'
 PERMIT_TEMPLATE = 'assets/reglement_2017.pdf'
 PERMITS_DIR = 'permits'
-SENDGRID_API_KEY = SENDGRID_KEY
+SENDGRID_API_KEY = os.environ.get('SENDGRID_KEY')
 WEBAPP_URL = 'http://149.202.44.29/capel-client'
 SERVER_URL = 'https://capel-beta.herokuapp.com/'
