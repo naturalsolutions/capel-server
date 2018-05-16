e67cbfc3677399cd1aa85a847d8793336d7ce6c5
et-taousy <zouhirtaousy@gmail.com>
2018-05-16 14:47:46 +0200

    conf mig

--- migrations/env.py
+++ migrations/env.py
@@ -28,20 +28,28 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # ... etc.
 
 
+def include_object(object, name, type_, reflected, compare_to):
+    if (name == 'spatial_ref_sys'):
+        return False
+    else:
+        return True
+
+
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+    context.configure(
+        url=url,
+        include_object=include_object,
+        compare_type=True
+    )
 
     with context.begin_transaction():
         context.run_migrations()
@@ -49,10 +57,8 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     # this callback is used to prevent an auto-migration from being generated
@@ -73,6 +79,8 @@ def run_migrations_online():
     context.configure(connection=connection,
                       target_metadata=target_metadata,
                       process_revision_directives=process_revision_directives,
+                      include_object=include_object,
+                      compare_type=True,
                       **current_app.extensions['migrate'].configure_args)
 
     try:
