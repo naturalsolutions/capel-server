09ed62b98b9404ead130b2e648f1c741dc750ad7
et-taousy <zouhirtaousy@gmail.com>
2018-08-23 23:00:36 +0200

    wip

--- api/permits/__init__.py
+++ api/permits/__init__.py
@@ -37,9 +37,8 @@ def get_permit(id):
         my_file = Path(permit_model)
         if not my_file.is_file():
             with open(permit_model, 'wb') as fout:
-                fout.write(base64.decodestring((bytes(typePermit.template, 'utf-8'))))
+                fout.write(base64.decodestring((bytes(typePermit.template))))
         permit =  Permit()
-        """
         instance = db.session.query(Permit).filter_by(user_id = id, typepermit_id = typePermit.id).first()
         if  instance is None:
             permit.user_id = id
@@ -57,7 +56,7 @@ def get_permit(id):
             sendmail(
                 'no-reply@natural-solutions.eu', user.email,
                 current_app.config['PERMIT_SIGNED_SUBJECT'], emailBody)
-        """
+
         if user is not None:
             now = datetime.datetime.utcnow()
             f = '/'.join([
