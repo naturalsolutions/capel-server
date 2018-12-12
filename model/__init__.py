3bc9958b28e2c27d951bc7552f288b8f3e3ead85
pat <patkap@users.noreply.github.com>
2018-12-12 10:32:05 +0100

    lint model

--- model/__init__.py
+++ model/__init__.py
@@ -24,6 +24,7 @@ def unique_constraint_key(error):
     m = re.search(DUPLICATE_KEY_ERROR_REGEX, error)
     return m.group('duplicate_key')
 
+
 def not_null_constraint_error(error):
     print('la')
     print(error)
@@ -34,7 +35,7 @@ def not_null_constraint_error(error):
     regexp_table = r'INSERT INTO (?P<table>.*)'
     match_table = re.search(regexp_table, error)
     table = match_table.group('table').split(' ')[0]
-    
+
     return {
         'table': table,
         'column': column,
@@ -42,12 +43,13 @@ def not_null_constraint_error(error):
         'name': 'missing_attribute'
     }
 
+
 def unique_constraint_error(error):
-    regexp_detail = r'DETAIL:\s+Key \((?P<column>.*)\)=\((?P<value>.*)\) already exists.'
+    regexp_detail = r'DETAIL:\s+Key \((?P<column>.*)\)=\((?P<value>.*)\) already exists.'  # noqa: E501
     match_detail = re.search(regexp_detail, error)
     column = match_detail.group('column')
     value = match_detail.group('value')
-    regexp_table = r'violates unique constraint "(?P<table>.*)_' + re.escape(column) + r'_key"'
+    regexp_table = r'violates unique constraint "(?P<table>.*)_' + re.escape(column) + r'_key"'  # noqa: E501
     match_table = re.search(regexp_table, error)
     table = match_table.group('table')
     return {
@@ -57,12 +59,13 @@ def unique_constraint_error(error):
         'name': 'value_exists'
     }
 
+
 class User(db.Model):
 
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)  # noqa: A003
     username = db.Column(db.String(64), unique=True, nullable=True)
     password = db.Column(db.String(255), nullable=True)
     email = db.Column(db.String(255), unique=True, nullable=True)
@@ -70,7 +73,7 @@ class User(db.Model):
     address = db.Column(db.Text, nullable=True)
     country = db.Column(db.String(255), nullable=True)
     company = db.Column(db.String(255), nullable=True)
-    zip = db.Column(db.String(255), nullable=True)
+    zip = db.Column(db.String(255), nullable=True)  # noqa: A003
     common = db.Column(db.String(255), nullable=True)
     city = db.Column(db.String(255), nullable=True)
     phone = db.Column(db.String(255), nullable=True)
@@ -80,13 +83,13 @@ class User(db.Model):
     status = db.Column(db.String(255), nullable=True)
     review = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)  # noqa: E501
     role = db.Column(db.String(255), nullable=True)
     photo = db.Column(db.Text())
     boats = db.relationship('Boat', backref='users', lazy='dynamic')
     offenses = db.relationship('Offense', backref='users', lazy='dynamic')
     diveSites = db.relationship('DiveSite', backref='users', lazy='dynamic')
-    dives = db.relationship('Dive', backref='users', lazy='dynamic', foreign_keys='Dive.user_id')
+    dives = db.relationship('Dive', backref='users', lazy='dynamic', foreign_keys='Dive.user_id')  # noqa: E501
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -112,7 +115,10 @@ class User(db.Model):
             'review': self.review,
             'photo': self.photo,
             'created_at': self.created_at,
-            'boats': [boat.json() for boat in self.boats if boat.status != 'removed'],
+            'boats': [
+                boat.json()
+                for boat in self.boats
+                if boat.status != 'removed'],
             'offenses': [offense.json() for offense in self.offenses],
             'nbr_dives': self.dives.count()
         }
@@ -123,7 +129,7 @@ class Boat(db.Model):
     __tablename__ = 'boats'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     name = db.Column(db.String(50), nullable=False)
     matriculation = db.Column(db.Unicode(255), unique=True)
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=True)
@@ -140,12 +146,13 @@ class Boat(db.Model):
             'status': self.status
         }
 
+
 class Offense(db.Model):
 
     __tablename__ = 'offenses'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     start_at = db.Column(db.DateTime)
     end_at = db.Column(db.DateTime)
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=True)
@@ -162,31 +169,30 @@ class Offense(db.Model):
             'status': self.status
         }
 
+
 class Permit(db.Model):
 
     __tablename__ = 'permits'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     status = db.Column(db.Unicode(255))
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))  # noqa: E501
     user = db.relationship('User', foreign_keys='Permit.user_id')
-    typepermit_id = db.Column(db.Integer(), db.ForeignKey('typepermits.id'), nullable=True)
-    typepermit = db.relationship('TypePermit', foreign_keys='Permit.typepermit_id')
+    typepermit_id = db.Column(db.Integer(), db.ForeignKey('typepermits.id'), nullable=True)  # noqa: E501
+    typepermit = db.relationship('TypePermit', foreign_keys='Permit.typepermit_id')  # noqa: E501
     created_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
 
     def json(self):
         return {
             'id': self.id,
-            'status': self.status,
             'user': self.user.json(),
-            'typepermit':self.typepermit.json(),
+            'typepermit': self.typepermit.json(),
             'status': self.status,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
-
 
 
 class TypeDive(db.Model):
@@ -197,7 +203,7 @@ class TypeDive(db.Model):
     # {"id": 16, "name": "Apnée"}
     __tablename__ = 'typedives'
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     name = db.Column(db.Unicode(255), nullable=False, unique=True)
 
     def __init__(self, name):
@@ -212,18 +218,19 @@ class TypeDive(db.Model):
             'name': self.name
         }
 
+
 class TypePermit(db.Model):
 
     __tablename__ = 'typepermits'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     start_at = db.Column(db.DateTime)
     end_at = db.Column(db.DateTime)
     template = db.Column(db.Text())
     status = db.Column(db.Unicode(255))
     caption = db.Column(db.Unicode(255))
-    dive_sites = db.relationship('DiveSite', secondary='typepermithearts', backref='type_permit')
+    dive_sites = db.relationship('DiveSite', secondary='typepermithearts', backref='type_permit')  # noqa: E501
 
     def json(self):
         return {
@@ -233,18 +240,20 @@ class TypePermit(db.Model):
             'template': self.template,
             'status': self.status,
             'caption': self.caption,
-            'dive_sites': [dive_site.cusJson() for dive_site in self.dive_sites]
+            'dive_sites': [
+                dive_site.cusJson() for dive_site in self.dive_sites]
         }
+
 
 class TypePermitHearts(db.Model):
     __tablename__ = 'typepermithearts'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
-    type_permit_id = db.Column(db.Integer(), db.ForeignKey('typepermits.id', ondelete='CASCADE'))
-    dive_site_id = db.Column(db.Integer(), db.ForeignKey('divesites.id', ondelete='CASCADE'))
-    dive_site = db.relationship("DiveSite", uselist=False, foreign_keys=[dive_site_id])
-    type_permit = db.relationship('TypePermit', uselist=False, foreign_keys=[type_permit_id])
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
+    type_permit_id = db.Column(db.Integer(), db.ForeignKey('typepermits.id', ondelete='CASCADE'))  # noqa: E501
+    dive_site_id = db.Column(db.Integer(), db.ForeignKey('divesites.id', ondelete='CASCADE'))  # noqa: E501
+    dive_site = db.relationship("DiveSite", uselist=False, foreign_keys=[dive_site_id])  # noqa: E501
+    type_permit = db.relationship('TypePermit', uselist=False, foreign_keys=[type_permit_id])  # noqa: E501
 
     def json(self):
         return {
@@ -259,7 +268,7 @@ class DiveSite(db.Model):
     __tablename__ = 'divesites'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     name = db.Column(db.String())
     referenced = db.Column(db.String)
     geom_mp = db.Column(Geometry('MULTIPOINT'))
@@ -270,8 +279,7 @@ class DiveSite(db.Model):
     status = db.Column(db.String())
     privacy = db.Column(db.String())
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=True)
-    heart_id = db.Column(db.Integer(), db.ForeignKey('divesites.id'), nullable=True)
-
+    heart_id = db.Column(db.Integer(), db.ForeignKey('divesites.id'), nullable=True)  # noqa: E501
 
     def all_sites():
 
@@ -306,7 +314,7 @@ class DiveSite(db.Model):
                    "user_id, "
                    "ST_AsGeoJSON(geom_poly) as geom_poly, "
                    "ST_AsGeoJSON(geom_mp) as geom_mp,"
-                     "privacy"
+                   "privacy"
                    " from divesites "
                    "where category = 'coeur' and status='enabled' ")
         result = db.engine.execute(sql)
@@ -316,7 +324,7 @@ class DiveSite(db.Model):
                              )
         return diveSites
 
-    def getHeartsByPoint(latitude, longitude):
+    def getHeartsByPoint(latitude: float, longitude: float):
 
         sql = text("select id, "
                    "name, "
@@ -330,7 +338,7 @@ class DiveSite(db.Model):
                    " privacy "
                    " from divesites "
                    "where category = 'coeur' and status='enabled' "
-                   "and  st_contains(geom_poly, ST_GeomFromText('POINT("+longitude+ " "+latitude+")', 4326))")
+                   "and  st_contains(geom_poly, ST_GeomFromText('POINT(" + float(longitude) + " " + float(latitude) + ")', 4326))")  # noqa: E501
         result = db.engine.execute(sql)
         diveSites = []
         for row in result:
@@ -351,7 +359,7 @@ class DiveSite(db.Model):
                    "ST_AsGeoJSON(geom_mp) as geom_mp,"
                    " privacy "
                    " from divesites "
-                   "where id in (select site_id from dives where user_id = '"+str(user.id)+"') "
+                   "where id in (select site_id from dives where user_id = '" + str(user.id) + "') "  # noqa: E501
                    )
         result = db.engine.execute(sql)
         diveSites = []
@@ -388,35 +396,33 @@ class DiveSite(db.Model):
         }
 
 
-
 class Dive(db.Model):
 
     __tablename__ = 'dives'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     date = db.Column(db.DateTime)
     times = db.Column(db.ARRAY(db.Time, dimensions=2))
 
     user_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=True)
-    user = db.relationship('User', back_populates='dives', foreign_keys='Dive.user_id')
+    user = db.relationship('User', back_populates='dives', foreign_keys='Dive.user_id')  # noqa: E501
     shop_id = db.Column(db.Integer(), db.ForeignKey('users.id'), nullable=True)
     shop = db.relationship("User", foreign_keys='Dive.shop_id')
 
     boats = db.relationship('Boat', secondary='diveboats', backref='dive')
-    #dive_types = db.relationship('TypeDive', secondary='divetypedives',  backref='dive')
+    # dive_types = db.relationship('TypeDive', secondary='divetypedives',  backref='dive')  # noqa: E501
 
-    site_id = db.Column(db.Integer(), db.ForeignKey('divesites.id'), nullable=True)
-    dive_site = db.relationship("DiveSite", uselist=False, foreign_keys=[site_id])
+    site_id = db.Column(db.Integer(), db.ForeignKey('divesites.id'), nullable=True)  # noqa: A003,E501
+    dive_site = db.relationship("DiveSite", uselist=False, foreign_keys=[site_id])  # noqa: A003,E501
 
     latitude = db.Column(db.String())
     longitude = db.Column(db.String())
     comment = db.Column(db.Text())
-    weather_id = db.Column(db.Integer(), db.ForeignKey('weathers.id', ondelete='CASCADE'))
-    weather = db.relationship("Weather", uselist=False, foreign_keys=[weather_id])
+    weather_id = db.Column(db.Integer(), db.ForeignKey('weathers.id', ondelete='CASCADE'))  # noqa: E501
+    weather = db.relationship("Weather", uselist=False, foreign_keys=[weather_id])  # noqa: E501
 
-    divetypedives = db.relationship('DiveTypeDive', backref='users', lazy='dynamic', foreign_keys='DiveTypeDive.dive_id')
-
+    divetypedives = db.relationship('DiveTypeDive', backref='users', lazy='dynamic', foreign_keys='DiveTypeDive.dive_id')  # noqa: E501
 
     def json(self):
 
@@ -426,11 +432,11 @@ class Dive(db.Model):
             'comment': self.comment,
             'weather': self.weather.json(),
             'boats': [boat.json() for boat in self.boats],
-            'times': [[time[0].__str__(), time[1].__str__()] for time in self.times],
+            'times': [[time[0].__str__(), time[1].__str__()] for time in self.times],  # noqa: E501
             'user': self.user.json(),
             'shop': self.shop.json() if self.shop else '',
             'dive_site': self.dive_site.cusJson(),
-            'divetypedives': [divetypedive.json() for divetypedive in self.divetypedives]
+            'divetypedives': [divetypedive.json() for divetypedive in self.divetypedives]  # noqa: E501
         }
 
 
@@ -440,7 +446,7 @@ class Weather(db.Model):
     __table_args__ = {'extend_existing': True}
 
     def __init__(self, sky, sea, wind,
-                 water_temperature = 0, wind_temperature = 0, visibility = 0):
+                 water_temperature=0, wind_temperature=0, visibility=0):
         self.sky = sky
         self.sea = sea
         self.wind = wind
@@ -448,7 +454,7 @@ class Weather(db.Model):
         self.wind_temperature = wind_temperature
         self.visibility = visibility
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
     sky = db.Column(db.String(255))
     sea = db.Column(db.String(255))
     wind = db.Column(db.String(255))
@@ -473,9 +479,9 @@ class DiveTypeDive(db.Model):
     __tablename__ = 'divetypedives'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
-    divetype_id = db.Column(db.Integer(), db.ForeignKey('typedives.id', ondelete='CASCADE'))
-    dive_id = db.Column(db.Integer(), db.ForeignKey('dives.id', ondelete='CASCADE'))
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
+    divetype_id = db.Column(db.Integer(), db.ForeignKey('typedives.id', ondelete='CASCADE'))  # noqa: E501
+    dive_id = db.Column(db.Integer(), db.ForeignKey('dives.id', ondelete='CASCADE'))  # noqa: E501
     divers = db.Column(db.Integer())
     dive = db.relationship('Dive')
     typeDive = db.relationship('TypeDive')
@@ -493,9 +499,9 @@ class DiveBoat(db.Model):
     __tablename__ = 'diveboats'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer(), primary_key=True)
-    dive_id = db.Column(db.Integer(), db.ForeignKey('dives.id', ondelete='CASCADE'))
-    boat_id = db.Column(db.Integer(), db.ForeignKey('boats.id', ondelete='CASCADE'))
+    id = db.Column(db.Integer(), primary_key=True)  # noqa: A003
+    dive_id = db.Column(db.Integer(), db.ForeignKey('dives.id', ondelete='CASCADE'))  # noqa: E501
+    boat_id = db.Column(db.Integer(), db.ForeignKey('boats.id', ondelete='CASCADE'))  # noqa: E501
 
     dive = db.relationship('Dive')
     boat = db.relationship('Boat')
